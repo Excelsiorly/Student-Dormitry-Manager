f79e7c0ff50d87432cec9667d36088cbf2de4784
Excelsiorly <61625711+Excelsiorly@users.noreply.github.com>
2020-03-26 21:13:11 +0800

    主程序
    学生宿舍管理

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"menu.h"
+using namespace std;
+
+/* 宿舍基本信息模块：添加、修改、查询、删除宿舍基本信息
+
+	学生入住模块：学生基本信息、缴费、入住、退房信息等*/
+
+int main() {
+	main_menu();
+	system("pause");
+	return 0;
+}
